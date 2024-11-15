ffd89ff77f94a88330a1e991c936b25028aea36d
Mark Skelton <mdskelton99@gmail.com>
2024-11-14 20:10:48 -0600

    JSON

--- pkg/parser/tsv.go
+++ pkg/parser/tsv.go
@@ -2,8 +2,10 @@ package parser
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -37,24 +39,24 @@ func ParseTSV(r io.Reader, hasHeader bool) []map[string]string {
 }
 
 func ParseJson(r io.Reader) []map[string]string {
-	return []map[string]string{}
-	// rows := []map[string]string{}
-	// scanner := bufio.NewScanner(r)
-	//
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// 	cells := map[string]string{}
-	//
-	// 	for i, cell := range strings.Split(line, "\t") {
-	// 		cells[strconv.Itoa(i)] = cell
-	// 	}
-	//
-	// 	rows = append(rows, cells)
-	// }
-	//
-	// if err := scanner.Err(); err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
-	// }
-	//
-	// return rows
+	var rawRows []map[string]any
+	decoder := json.NewDecoder(r)
+
+	if err := decoder.Decode(&rawRows); err != nil {
+		log.Fatalf("Error reading input: %v\n", err)
+		return nil
+	}
+
+	var rows []map[string]string
+	for _, rawRow := range rawRows {
+		row := make(map[string]string)
+
+		for key, value := range rawRow {
+			row[key] = fmt.Sprintf("%v", value) // Convert any value to a string
+		}
+
+		rows = append(rows, row)
+	}
+
+	return rows
 }
